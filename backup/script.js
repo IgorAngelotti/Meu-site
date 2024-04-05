df47b96a4ce00ef09e43a07c62ba37b287d0ee6c
IgorAngelotti <lanzaigor@live.com>
2024-04-05 14:19:27 -0300

    pagina 2 alterada

---
+++ backup/script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        if (display.value === "") {
+            return
+        } else {
+            display.value = eval(display.value);
+        }
+        display.value = eval(display.value);
+    } catch (error) {
+        return
+    }
+}
+
+
+function apagarUltimoDigito() {
+    display.value = display.value.slice(0, -1)
+}
